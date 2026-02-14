8f089906d63648b506871194332fcbf335d885bc
Sauce-jpg <dannecorleone@hotmail.com>
2026-02-15 00:26:11 +0100

    Update page.tsx

--- app/pathfinder/[campaignId]/characters/[characterId]/page.tsx
+++ app/pathfinder/[campaignId]/characters/[characterId]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import { supabase } from "../../../../../../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 import Link from "next/link";
 
 // Utility functions
