0e16482e80c111c32721f91c4aafa557ca853ac4
Sauce-jpg <dannecorleone@hotmail.com>
2026-02-19 17:41:08 +0100

    Update WeaponsTab.tsx

--- components/inventory/WeaponsTab.tsx
+++ components/inventory/WeaponsTab.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import EquipmentBrowser from "../EquipmentBrowser";
 import { mapWeaponToCharacter } from "@/lib/equipmentMappers";
-import "../styles/EquipmentBrowser.css";
+import "../../styles/EquipmentBrowser.css";
 
 interface WeaponsTabProps {
   characterId: string;
